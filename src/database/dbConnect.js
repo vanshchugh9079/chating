bdd317e4ca3182d6dd28a02d762711d8b26704be
vanshchugh9079 <vanshchugh67@gmail.com>
2025-02-28 12:41:05 +0530

    second-commit

--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -13,6 +13,7 @@ const dbConnect = async () => {
   try {
     // Connect to MongoDB using the URI from the environment variable
     await mongoose.connect(process.env.URI, clientOptions);
+    console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection failed:", error.message);
     throw new Error(error);
