33b0757dd8daa4c9c6eb35d1d7afd010f68f566e
unknown <vanshch9079@gmail.com>
2025-06-27 12:24:10 +0530

    update all

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(helmet({
 // setup  Middleware
 app.use(express.json());
 app.use(cors({
-    origin:"*",
+    origin:"https://chatfight.netlify.app",
     credentials: true
 }))
 app.use(cookieParser())
